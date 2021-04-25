7bbe3d18bffc13fe4f01c5884b66a4eb8b8fab43
sree-chikati <srechi453.fusdk12@gmail.com>
2021-04-25 16:41:50 -0700

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const express = require('express');
+
 // App Setup
 const app = express();
 
-// Allows us to use content from Public file
-app.use(express.static('public'));
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); // Add after body parser initialization!
 
 // Middleware
 const exphbs  = require('express-handlebars');
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+
+// All requires
+require('./controllers/posts.js')(app);
+require('./data/reddit-db'); // Set db
+
 
 // Routes
 app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/posts/new', (req, res) => {
+    console.log('Loading posts-new')
+    res.render('posts-new');
+})
+
 app.listen(3000, () => {
-    console.log('Gif Search listening on port localhost:3000!');
+    console.log('Reddit-Clone listening on port localhost:3000!');
 });
 
