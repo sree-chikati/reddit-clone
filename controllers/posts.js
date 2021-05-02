c23d8ba7052d7233c2efab1324cab361dbed28e7
sree-chikati <srechi453.fusdk12@gmail.com>
2021-05-02 15:51:11 -0700

    Users must be logged in to submit a post

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,9 +4,10 @@ module.exports = (app) => {
     
     // get all posts
     app.get('/', (req, res) => {
+      var currentUser = req.user;
       Post.find({}).lean()
         .then(posts => {
-          res.render('posts-index', { posts });
+          res.render('posts-index', { posts , currentUser});
         })
         .catch(err => {
           console.log(err.message);
@@ -14,27 +15,31 @@ module.exports = (app) => {
     })
 
     app.get('/posts/new', (req, res) => {
+      var currentUser = req.user;
       console.log('Loading posts-new')
-      res.render('posts-new');
+      res.render('posts-new', { currentUser });
     })
 
     // CREATE
     app.post('/posts/new', (req, res) => {
-      // INSTANTIATE INSTANCE OF POST MODEL
-      const post = new Post(req.body);
-  
-      // SAVE INSTANCE OF POST MODEL TO DB
-      post.save((err, post) => {
-        // REDIRECT TO THE ROOT
-        return res.redirect('/');
-      })
+      if(req.user){
+        const post = new Post(req.body);
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+          return res.redirect('/');
+        })
+      }
+      else {
+        return res.status(401); // UNAUTHORIZED
+      }
     });
 
     // SUBREDDIT
     app.get("/n/:subreddit", function(req, res) {
+      var currentUser = req.user;
       Post.find({ subreddit: req.params.subreddit }).lean()
         .then(posts => {
-          res.render("posts-index", { posts });
+          res.render("posts-index", { posts, currentUser });
         })
         .catch(err => {
           console.log(err);
@@ -43,10 +48,11 @@ module.exports = (app) => {
 
     // can search post by id
     app.get("/posts/:id", function(req, res) {
+        var currentUser = req.user;
         // LOOK UP THE POST
         Post.findById(req.params.id).lean().populate('comments')
         .then((post) => {
-          res.render('posts-show', { post })
+          res.render('posts-show', { post, currentUser })
         })
         .catch((err) => {
           console.log(err.message)
--- server.js
+++ server.js
@@ -8,13 +8,28 @@ const jwt = require('jsonwebtoken');
 // App Setup
 const app = express();
 
-// Use Body Parser
+// Middleware
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken == "undefined" || req.cookies.nToken === null) {
+      console.log("No valid cookie found");
+      req.user = null;
+    } else {
+      var token = req.cookies.nToken;
+      var decodedToken = jwt.decode(token, { complete: true }) || {};
+      req.user = decodedToken.payload;
+    }
+    next();
+};
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator()); // Add after body parser initialization!
 app.use(cookieParser());
+app.use(checkAuth);
 
-// Middleware
+
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
