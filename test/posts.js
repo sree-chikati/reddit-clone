d2b638087175c9c855427e75b4c2fe3f99ab11e0
sree-chikati <srechi453.fusdk12@gmail.com>
2021-05-02 17:34:11 -0700

    Fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -1,42 +1,63 @@
-// test/posts.js
-const app = require("./../server");
+//test/posts.js
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const server = require("../server");
 const expect = chai.expect;
-
-// Import the Post model from our models folder so we
-// we can use it in our tests.
-const Post = require('../models/post');
-const server = require('../server');
-
-chai.should();
+const should = chai.should();
 chai.use(chaiHttp);
 
+// Agent that will keep track of our cookies
+const agent = chai.request.agent(server);
+
+
+// CODE STARTS HERE
+const Post = require('../models/post');
+const User = require("../models/user");
+
 describe('Posts', function() {
-  const agent = chai.request.agent(server);
-  // Post that we'll use for testing purposes
-  const newPost = {
-      title: 'post title',
-      url: 'https://www.google.com',
-      summary: 'post summary'
-  };
+    // Post that we'll use for testing purposes
+    const newPost = {
+        title: 'post title',
+        url: 'https://www.google.com',
+        summary: 'post summary',
+        subreddit: 'subreddit'
+    };
+
+    const user = {
+        username: 'poststest',
+        password: 'testposts'
+    }
+
+    before(function (done) {
+        agent
+        .post('/sign-up')
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send(user)
+        .then(function(res) {
+            done();
+        })
+        .catch(function(err) {
+            done(err);
+        });
+    });
 
   it('Should create with valid attributes at POST /posts/new', function(done) {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
-        .then(function (initialDocCount) { agent
+        .then(function (initialDocCount) {
+            agent
             .post("/posts/new")
-            // This line fakes a form post,
-            // since we're not actually filling out a form
+            //This line fakes a form post,
+            //since we're not actually filing out a form
             .set("content-type", "application/x-www-form-urlencoded")
-            // Make a request to create another
+            //make a request to create another
             .send(newPost)
             .then(function (res) {
                 Post.estimatedDocumentCount()
                 .then(function (newDocCount) {
-                    // Check that the database has one more post in it
+                    //Check that hte database has one more post in it
                     expect(res).to.have.status(200);
-                    // Check that the database has one more post in it
+                    //Check that the database has one more post in it
                     expect(newDocCount).to.be.equal(initialDocCount + 1)
                     done();
                 })
@@ -49,11 +70,27 @@ describe('Posts', function() {
             });
         })
         .catch(function (err) {
-            done(err);
+            done(err)
         });
     });
 
-    after(function () {
-        Post.findOneAndDelete(newPost);
-      });
+    after(function (done) {
+        Post.findOneAndDelete(newPost)
+        .then(function (res) {
+            agent.close()
+      
+            User.findOneAndDelete({
+                username: user.username
+            })
+              .then(function (res) {
+                  done()
+              })
+              .catch(function (err) {
+                  done(err);
+              });
+        })
+        .catch(function (err) {
+            done(err);
+        });
+    });
 });
